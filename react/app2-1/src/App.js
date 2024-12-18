d73ccf525e937f77ace6cd3341f645a5ea67fc01
OFF <peerapat13th@gmail.com>
2024-12-18 12:09:50 +0700

    app2-1 done

--- react/app2-1/src/App.js
+++ react/app2-1/src/App.js
@@ -1,17 +1,33 @@
-
 import './App.css';
 import React from 'react';
 import { Header, Content, Footer } from './func-components';
 import Calendar from './class-components';
+import MsgBox from './class-props';
+import MsgBox2 from './func-props';
 
 function App() {
   return (
     <>
       <Header />
-      <p style={{textAlign: 'center'}}><Calendar/></p>
+      <p style={{ textAlign: 'center' }}><Calendar /></p>
       <Content />
       <Footer />
+      <MsgBox
+        text="Hello, World!"
+        color="green"
+        bgcolor="black"
+        fontSize="20px"
+        border="1px solid black"
+      />
+      <MsgBox2
+        text="How are you today?"
+        color="blue"
+        bgcolor="white"
+        fontSize="24px"
+        border="1px solid blue"
+      />
     </>
+
   );
 }
 
---
+++ react/app2-1/src/class-props.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+export default class MsgBox extends Component {
+
+    render() {
+        const divStyle = {
+            margin: '10px',
+            padding: '5px',
+            color: this.props.color,
+            backgroundColor: this.props.bgcolor,
+            fontSize: this.props.fontSize,
+            border: this.props.border
+            
+        };
+        return (
+            <div style={divStyle}>
+                {this.props.text}
+            </div>
+        );
+    }
+}
