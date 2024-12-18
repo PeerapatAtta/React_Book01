ad7cfcb6306600a1a83dce04c9b3b76f1f4abcd4
OFF <peerapat13th@gmail.com>
2024-12-18 11:39:12 +0700

    add class-components.js

--- react/app2-1/src/App.js
+++ react/app2-1/src/App.js
@@ -2,10 +2,16 @@
 import './App.css';
 import React from 'react';
 import { Header, Content, Footer } from './func-components';
+import Calendar from './class-components';
 
 function App() {
   return (
-    [<Header/>,<Content/>,<Footer/>]
+    <>
+      <Header />
+      <p style={{textAlign: 'center'}}><Calendar/></p>
+      <Content />
+      <Footer />
+    </>
   );
 }
 
