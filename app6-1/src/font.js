454a810bc52c9ed8f1d9803e8d768aa5f6212be6
OFF <peerapat13th@gmail.com>
2025-03-23 09:56:43 +0700

    app6-1 is done

---
+++ app6-1/src/font.js
@@ -0,0 +1,14 @@
+export default function Font() {
+    return (
+        <div className=" border p-3" style={{ width: '400px', margin: '10px auto' }}>
+            <div className="fw-normal">Font Weight Normal - ปกติ</div>
+            <div className="fw-light">Font Weight Light - บาง</div>
+            <div className="fw-bold">Font Weight Bold - หนา</div>
+            <div className="fst-italic">Font Italic - เอียง</div>
+            <div className="fw-bold fst-italic">
+                Font Weight Bold + Italic - หนาเอียง
+            </div>
+        </div>
+       
+    );
+}
